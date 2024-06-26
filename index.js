adafa4438f94376e07cd617d7779e076c10f0a82
Mark <mark.njau@student.moringaschool.com>
2024-06-26 23:09:39 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+// Declare to uppercase the customerName variable
+var customerName = 'bob';
+
+// Function to uppercase the CustomerName variable
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// Function to set the bestCustomer variable
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+
+//Function to overwrite the bestCustomer variable
+function overwriteBestCustomer(newBestCustomer) {
+    bestCustomer = newBestCustomer; 
+}
+
+// Declare a constant variable leastFavoriteCustomer
+const leastFavoriteCustomer = 'initial value';
+
+// Function to attempt changing the leastFavoriteCustomer variable
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new value'; //This should throw an error
+}
+
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,6 +1,6 @@
 require ( './helpers.js' );
 
-describe('Fix the Scope', function() {
+ describe('Fix the Scope', function() {
   describe('declare customerName to be bob in global scope', function() {
     it('returns the customerName', function() {
       expect(window.customerName).to.equal('bob');
